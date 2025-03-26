7531e686342521112e3debc242b4063454aba756
gagewilson814 <gagewilson814@u.boisestate.edu>
2025-03-26 01:26:33 -0600

    starter code

--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,16 @@
-/**Update this file with the starter code**/
+#include "lab.h"
+#include <errno.h>
+
+size_t btok(size_t bytes) {}
+
+struct avail *buddy_calc(struct buddy_pool *pool, struct avail *buddy) {}
+
+void *buddy_malloc(struct buddy_pool *pool, size_t size) {}
+
+void buddy_free(struct buddy_pool *pool, void *ptr) {}
+
+void *buddy_realloc(struct buddy_pool *pool, void *ptr, size_t size) {}
+
+void buddy_init(struct buddy_pool *pool, size_t size) {}
+
+void buddy_destroy(struct buddy_pool *pool) {}
